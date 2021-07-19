b43e14c76fdf1bb1ae3b464c20fa09a3f7147b98
Elad.b.a <eladb21@gmail.com>
2021-07-20 01:01:48 +0300

    Initial commit

---
+++ HandVol.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+import HandTrackingModule as htm
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+wCam, hCam = 640, 480
+
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+detector = htm.handDetector(detectionCon=0.7)
+
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+volRange = volume.GetVolumeRange()
+minVol = volRange[0]
+maxVol = volRange[1]
+
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        cv2.circle(img, (x1, y1), 15, (0, 255, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (0, 255, 255), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        cv2.circle(img, (cx, cy), 15, (0, 255, 255), cv2.FILLED)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        # Hand range 50 - 300
+        # Volume Range -65 - 0
+
+        vol = np.interp(length, [50, 300], [minVol, maxVol])
+        volume.SetMasterVolumeLevel(vol, None)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
